7242b596ed402209464cee2dd941b0ae009d6b9b
jyoti112421 <141920398+jyoti112421@users.noreply.github.com>
2024-08-31 23:55:43 +0530

    Time: 12 ms (6.47%) | Memory: 45.3 MB (47.56%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int low =0 , high =s.length()-1,count1=0, count2 =0;
+        while(low<high){
+            if(s.charAt(low)==s.charAt(high)){
+                low++;
+                high--;
+            } else {
+                count1++; low++;
+            }
+            }
+            low=0; high =s.length()-1;
+            while(low<high){
+                if(s.charAt(low)==s.charAt(high)){
+                    low++;high--;
+                }
+                else{
+                    count2++; high--;
+                }
+            }
+            if(count1==0 || count1==1 || count2==0 || count2==1) return true;
+            return false;
+        }
+    }
